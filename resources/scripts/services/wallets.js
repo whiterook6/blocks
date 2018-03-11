daf38f37807c880364826df38ade7b7cb989804e
whiterook6 <whiterook6@gmail.com>
2018-03-11 16:37:42 -0700

    fixing typo in fade-out, and also trying to get the wallets to fade in as well as slide in

--- resources/scripts/services/wallets.js
+++ resources/scripts/services/wallets.js
@@ -1,35 +1,41 @@
 class Wallets {
-	constructor() {
+	constructor($timeout) {
+		this.$timeout = $timeout;
 		this.data = [];
+		this.outstanding_requests = 0;
 	}
 
 	refresh(){
+		this.outstanding_requests++;
 		return new Promise((resolve, reject) => {
-			this.data = [{
-				id: 1,
-				name: 'Bitcoin',
-				source: 'Coinomi',
-				current_balance: 0.3417,
-				current_value: {
-					usd: 4134.77,
-					cad: 5296.91
-				},
-			}, {
-				id: 2,
-				name: 'Ethereum',
-				source: 'Coinomi',
-				current_balance: 1.6501,
-				current_value: {
-					usd: 1197.99,
-					cad: 1534.70
-				},
-			}];
+			this.$timeout(() => {
+				this.outstanding_requests--;
+				this.data = [{
+					id: 1,
+					name: 'Bitcoin',
+					source: 'Coinomi',
+					current_balance: 0.3417,
+					current_value: {
+						usd: 4134.77,
+						cad: 5296.91
+					},
+				}, {
+					id: 2,
+					name: 'Ethereum',
+					source: 'Coinomi',
+					current_balance: 1.6501,
+					current_value: {
+						usd: 1197.99,
+						cad: 1534.70
+					},
+				}];
 
-			resolve(this.data);
+				resolve(this.data);
+			}, 5000);
 		});
 	}
 }
 
-Wallets.$inject = [];
+Wallets.$inject = ['$timeout'];
 
 export default Wallets;
